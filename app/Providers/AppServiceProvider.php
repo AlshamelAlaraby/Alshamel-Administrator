744a73c170406f3332ef75fdc5ee1e332d8f0d57
Mostafa1712002 <ebrahimmostafa695@gmail.com>
2022-11-22 16:15:49 +0200

    add moduels

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,10 +2,12 @@
 
 namespace App\Providers;
 
-use App\Repositories\User\UserRepository;
-use App\Repositories\User\UserRepositoryInterface;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
+use App\Repositories\User\UserRepository;
+use App\Repositories\Module\ModuleInterface;
+use App\Repositories\Module\ModuleRepository;
+use App\Repositories\User\UserRepositoryInterface;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -17,6 +19,8 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         $this->app->bind(UserRepositoryInterface::class, UserRepository::class);
+
+        $this->app->bind(ModuleInterface::class, ModuleRepository::class);
     }
 
     /**
