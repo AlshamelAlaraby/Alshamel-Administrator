b5ff467ce9a640b27e2a180a998b786238470b6b
Ehab Barakat <95265260+ehab0barakat@users.noreply.github.com>
2022-11-22 18:58:31 +0200

    start

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Company\CompanyController;
+use App\Http\Controllers\Store\StoreController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +17,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+
+
+Route::middleware('auth:sanctum')->group(function () {
+
+    Route::apiResource('companies', CompanyController::class);
+    
+    Route::apiResource('stores', StoreController::class);
+    
 });
+
