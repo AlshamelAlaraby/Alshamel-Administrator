eb05cea05dc7fb52ed91e2560d79277a4ac11e6f
Ehab Barakat <95265260+ehab0barakat@users.noreply.github.com>
2022-11-29 18:57:54 +0200

    fix

---
+++ app/Repositories/Partner/PartnerRepository.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Repositories\Partner;
+
+use App\Models\Partner;
+use Illuminate\Support\Facades\DB;
+
+class PartnerRepository implements PartnerRepositoryInterface
+{
+
+    private $model;
+    public function __construct(Partner $model)
+    {
+        $this->model = $model;
+
+    }
+
+    public function getAllPartners($request)
+    {
+        $models = $this->model->where(function ($q) use ($request) {
+
+            if ($request->search) {
+                $q->where('name', 'like', '%' . $request->search . '%');
+                $q->orWhere('name_e', 'like', '%' . $request->search . '%');
+            }
+
+            if ($request->is_active) {
+                $q->where('is_active', $request->is_active);
+            }
+
+        })->latest();
+
+        if ($request->per_page) {
+            return ['data' => $models->paginate($request->per_page), 'paginate' => true];
+        } else {
+            return ['data' => $models->get(), 'paginate' => false];
+        }
+    }
+
+    public function find($id)
+    {
+        return $this->model->find($id);
+    }
+
+    public function create($request)
+    {
+        DB::transaction(function () use ($request) {
+            $this->model->create($request->all());
+            cacheForget("Partners");
+        });
+    }
+
+    public function update($request, $id)
+    {
+        DB::transaction(function () use ($id, $request) {
+            $this->model->where("id", $id)->update($request->all());
+            $this->forget($id);
+
+        });
+
+    }
+
+    public function delete($id)
+    {
+        $model = $this->find($id);
+        $this->forget($id);
+        $model->delete();
+    }
+
+
+    private function forget($id)
+    {
+        $keys = [
+            "Partners",
+            "Partners_" . $id,
+        ];
+        foreach ($keys as $key) {
+            cacheForget($key);
+        }
+
+    }
+}
