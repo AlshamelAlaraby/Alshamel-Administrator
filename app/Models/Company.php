94865c33726ea42a8ec280b7f3309142e431b7a9
Mostafa1712002 <mostafaibrahim1712002@gmail.com>
2022-12-13 15:20:58 +0200

    finish

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -2,15 +2,10 @@
 
 namespace App\Models;
 
-use App\Models\Module;
-use App\Models\Partner;
-use App\Models\Store;
-
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Support\Facades\Storage;
-use Spatie\Activitylog\Contracts\Activity;
 use Spatie\Activitylog\LogOptions;
 use Spatie\Activitylog\Traits\CausesActivity;
 use Spatie\Activitylog\Traits\LogsActivity;
@@ -20,7 +15,7 @@ class Company extends Model
     use HasFactory;
     use SoftDeletes, LogsActivity, CausesActivity;
 
-    protected $guarded = ['id'];
+    protected $guarded = [];
 
     protected $casts = [
         'is_active' => 'App\Enums\IsActive',
@@ -44,109 +39,6 @@ class Company extends Model
     public function getPhotoUrlAttribute()
     {
         return Storage::disk("companies")->url($this->logo);
-    }
-
-    public function scopeFilter($query, $request)
-    {
-        return $query->where(function ($q) use ($request) {
-
-            if ($request->search) {
-
-                $q->whereHas('partner', function ($q) use ($request) {
-                    $q->where('name', 'like', '%' . $request->search . '%');;
-                });
-
-                $q->orWhereHas('partner', function ($q) use ($request) {
-                    $q->where('name_e', 'like', '%' . $request->search . '%');;
-                });
-
-                $q->orWhere('name', 'like', '%' . $request->search . '%');
-                $q->orWhere('name_e', 'like', '%' . $request->search . '%');
-                $q->orWhere('url', 'like', '%' . $request->search . '%');
-                $q->orWhere('address', 'like', '%' . $request->search . '%');
-                $q->orWhere('phone', 'like', '%' . $request->search . '%');
-                $q->orWhere('cr', 'like', '%' . $request->search . '%');
-                $q->orWhere('tax_id', 'like', '%' . $request->search . '%');
-                $q->orWhere('vat_no', 'like', '%' . $request->search . '%');
-                $q->orWhere('email', 'like', '%' . $request->search . '%');
-                $q->orWhere('website', 'like', '%' . $request->search . '%');
-            }
-            if ($request->search && $request->columns) {
-                foreach ($request->columns as $column) {
-                    $q->orWhere($column, 'like', '%' . $request->search . '%');
-                }
-
-            }
-
-            if ($request->name) {
-                $q->where('name', $request->name);
-            }
-
-            if ($request->name_e) {
-                $q->where('name_e', $request->name_e);
-            }
-            if ($request->url) {
-                $q->where('url', $request->url);
-            }
-            if ($request->address) {
-                $q->where('address', $request->address);
-            }
-            if ($request->phone) {
-                $q->where('phone', $request->phone);
-            }
-            if ($request->cr) {
-                $q->where('cr', $request->cr);
-            }
-            if ($request->tax_id) {
-                $q->where('tax_id', $request->tax_id);
-            }
-            if ($request->vat_no) {
-                $q->where('vat_no', $request->vat_no);
-            }
-            if ($request->email) {
-                $q->where('email', $request->email);
-            }
-            if ($request->website) {
-                $q->where('website', $request->website);
-            }
-
-            if ($request->is_default) {
-                $q->where('is_default', $request->is_default);
-            }
-
-            if ($request->column_name && $request->column_value) {
-
-                $dataSearch = explode(".", $request->column_name);
-                if (count($dataSearch) == 1) {
-                    $q->where($request->column_name, "like", '%' . $request->column_value . '%');
-                }
-
-                if (count($dataSearch) == 2) {
-                    $q->whereHas($dataSearch[0], function ($q) use ($request, $dataSearch) {
-                        $q->where($dataSearch[1], 'like', '%' . $request->column_value . '%');;
-                    });
-                }
-
-            }
-
-        });
-    }
-
-    public function scopeFilterCompanyModules($query, $request)
-    {
-        if ($request->search) {
-            return $query->with(['modules' => function ($q) use ($request) {
-                $q->where('name', 'like', '%' . $request->search . '%')
-                    ->orWhere('name_e', 'like', '%' . $request->search . '%');
-            }]);
-        }
-        return $query->with('modules');
-    }
-
-    public function tapActivity(Activity $activity, string $eventName)
-    {
-        $activity->causer_id = auth()->user()->id ?? 0;
-        $activity->causer_type = auth()->user()->role ?? "admin";
     }
 
     public function getActivitylogOptions(): LogOptions
