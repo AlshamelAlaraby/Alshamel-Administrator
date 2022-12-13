bbee91426dcb1ca75f9d976ae247fef5b7025bd3
Mostafa1712002 <ebrahimmostafa695@gmail.com>
2022-12-13 13:03:41 +0200

    add activiy

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -2,16 +2,23 @@
 
 namespace App\Models;
 
+use App\Models\Module;
+use App\Models\Partner;
+use App\Models\Store;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Support\Facades\Storage;
+use Spatie\Activitylog\Contracts\Activity;
+use Spatie\Activitylog\LogOptions;
+use Spatie\Activitylog\Traits\CausesActivity;
+use Spatie\Activitylog\Traits\LogsActivity;
 
 class Company extends Model
 {
 
-    use SoftDeletes;
+    use SoftDeletes, LogsActivity, CausesActivity;
 
-    protected $guarded = [] ;
+    protected $guarded = [];
 
     protected $casts = [
         'is_active' => 'App\Enums\IsActive',
@@ -32,10 +39,24 @@ class Company extends Model
         return $this->hasMany(Store::class);
     }
 
-
     public function getPhotoUrlAttribute()
     {
-        return Storage::disk("companies")->url($this->logo) ;
+        return Storage::disk("companies")->url($this->logo);
     }
 
+    public function tapActivity(Activity $activity, string $eventName)
+    {
+        $activity->causer_id = auth()->user()->id ?? 0;
+        $activity->causer_type = auth()->user()->role ?? "admin";
+    }
+
+    public function getActivitylogOptions(): LogOptions
+    {
+        $user = auth()->user()->id ?? "system";
+
+        return \Spatie\Activitylog\LogOptions::defaults()
+            ->logAll()
+            ->useLogName('Store')
+            ->setDescriptionForEvent(fn(string $eventName) => "This model has been {$eventName} by ($user)");
+    }
 }
